8d8d88b9e852fa41c5c3ab88975cb6b7362d4215
UsielOM <usiel31197@outlook.es>
2022-09-23 12:59:21 -0500

    Estrucutra basica

---
+++ src/my-element.js
@@ -0,0 +1,16 @@
+import { LitElement, html } from 'lit-element';
+
+export class MyElement extends LitElement {
+
+
+    static get properties() {
+        return {
+
+        };
+    }
+
+    render() {
+        return html ``;
+    }
+}
+customElements.define('my-element', MyElement);
