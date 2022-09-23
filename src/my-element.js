60b6f0b680a24a185688359c090751e77b402a21
UsielOM <usiel31197@outlook.es>
2022-09-23 13:19:45 -0500

    Implementacion de hasChanged

--- src/my-element.js
+++ src/my-element.js
@@ -5,12 +5,29 @@ export class MyElement extends LitElement {
 
     static get properties() {
         return {
-
+            value: {
+                hasChanged(newVal, oldVal) {
+                    const hasChanged = newVal % 2 == 1;
+                    console.log(`${newVal}, ${oldVal}, ${hasChanged}`);
+                    return hasChanged;
+                }
+            }
         };
+    }
+    constructor() {
+        super();
+        this.value = 1;
     }
 
     render() {
-        return html ``;
+        return html `
+        <p>${this.value}</p>
+        <button @click="${this.nuevoValor}">Nuevo valor </button>
+        `;
+    }
+
+    nuevoValor() {
+        this.value = Math.floor(Math.random() * 100);
     }
 }
 customElements.define('my-element', MyElement);
